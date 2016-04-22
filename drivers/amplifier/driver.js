797ae616765061a5ec0079287d5240668c4f08a0
Kerk en IT <info@kerkenit.nl>
2016-04-22 21:31:59 +0200

    Fixed input search field for better user experience

--- drivers/amplifier/driver.js
+++ drivers/amplifier/driver.js
@@ -180,7 +180,7 @@ function searchForInputsByValue(value) {
 	var tempItems = [];
 	for (var i = 0; i < possibleInputs.length; i++) {
 		var tempInput = possibleInputs[i];
-		if (tempInput.friendlyName.indexOf(value) >= 0) {
+		if (tempInput.friendlyName.toLowerCase().indexOf(value.toLowerCase()) >= 0) {
 			tempItems.push({
 				icon: "",
 				name: tempInput.friendlyName,
